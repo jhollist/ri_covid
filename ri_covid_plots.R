8e04134cb4f6e623ae5b20f2b2707777f78af035
Jeff Hollister <jeff.w.hollister@gmail.com>
2022-01-12 17:02:56 -0500

    RI DOH data plots

--- ri_covid_plots.R
+++ ri_covid_plots.R
@@ -4,14 +4,36 @@ library(zoo)
 library(ggplot2)
 library(lubridate)
 library(tsibble)
+library(plotly)
+library(tidyr)
+library(hrbrthemes)
+
+ri_pop <- 1097379
+ct_pop <- 3605944
+ma_pop <- 7029917
 
 us <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
-ri <- us %>%
-  filter(state == "Rhode Island") %>%
-  mutate(daily_cases = zoo::rollapply(.data$cases, 2, function(x) x[2] - x[1], fill = NA),
-         daily_deaths = zoo::rollapply(.data$deaths, 2, function(x) x[2] - x[1], fill = NA)) %>%
+sne <- us %>%
+  filter(state %in% c("Rhode Island", "Connecticut", "Massachusetts")) %>%
+  mutate(pop = case_when(state == "Rhode Island" ~
+                           ri_pop,
+                         state == "Connecticut" ~
+                           ct_pop,
+                         state == "Massachusetts" ~
+                           ma_pop)) %>%
+  group_by(state) %>%
+  mutate(daily_cases = zoo::rollapply(.data$cases, 2, 
+                                      function(x) x[2] - x[1], fill = NA),
+         daily_deaths = zoo::rollapply(.data$deaths, 2, 
+                                       function(x) x[2] - x[1], fill = NA),
+         case_fatality_rate = (daily_deaths/daily_cases) * 100,
+         daily_cases_per100k = (daily_cases/pop) * 100000,
+         daily_deaths_per100k = (daily_deaths/pop) * 100000) %>%
   filter(daily_cases > 0, daily_deaths > 0) %>%
-  mutate(percent_daily_cases = daily_cases/max(daily_cases, na.rm = TRUE),
+  mutate(three_day_avg_cases = zoo::rollapply(.data$daily_cases, 7, mean, fill = NA),
+         three_day_avg_deaths = zoo::rollapply(.data$daily_deaths, 7, mean, fill = NA),
+         three_day_avg_cfr = zoo::rollapply(.data$case_fatality_rate, 7, mean, fill = NA),
+         percent_daily_cases = daily_cases/max(daily_cases, na.rm = TRUE),
          percent_daily_deaths = daily_deaths/max(daily_deaths, na.rm = TRUE),
          percent_weekly_cases = zoo::rollapply(.data$percent_daily_cases, 7, 
                                                function(x) 
@@ -20,18 +42,49 @@ ri <- us %>%
          percent_weekly_deaths = zoo::rollapply(.data$percent_daily_deaths, 7, 
                                                function(x) 
                                                  mean(x, na.rm = TRUE), 
-                                               fill = NA))
+                                               fill = NA)) %>%
+  ungroup() 
          
 
-ri %>%
-  mutate(year_week = yearweek(date)) %>%
-  group_by(year_week) %>%
-  summarize(weekly_cases = sum(daily_cases), weekly_deaths = sum(daily_deaths)) %>%
-  ungroup() %>%
-  mutate(percent_cases = weekly_cases/max(weekly_cases, na.rm = TRUE),
-         percent_deaths = weekly_deaths/max(weekly_deaths, na.rm = TRUE)) %>%
-  ggplot(aes(x = year_week, y = weekly_cases)) +
-  geom_line(aes(x = year_week, y = percent_cases), color = "red", size = 1) +
-  geom_line(aes(x = year_week, y = percent_deaths), color = "blue", size = 1)
-  
+variable_lab <- c("three_day_avg_cases" = "3-Day Avg. Cases", 
+                  "three_day_avg_deaths" = "3-Day Avg. Deaths",
+                  "three_day_avg_cfr" = "3-day Avg. Fatality Rate")
 
+
+sne_plot <- sne %>%
+  filter(state == "Rhode Island", date > ymd("2021-1-01")) %>%
+  filter(date > ymd("2021-10-01")) %>%
+  select(date, state, three_day_avg_cases, three_day_avg_deaths, 
+         three_day_avg_cfr) %>%
+  pivot_longer(cols = three_day_avg_cases:three_day_avg_cfr, 
+               names_to = "variable", values_to = "value") %>%
+  ggplot(aes(x = date, y = value)) +
+  geom_line(size = 1, aes(color = variable), show.legend = FALSE) +
+  facet_grid(variable ~ state, scales = "free_y", 
+             labeller = labeller(variable = variable_lab)) +
+  labs(x = "Date", y = "", title = "MA, CT, RI COVID-19") +
+  theme_ipsum_rc() +
+  scale_color_manual(values = c("three_day_avg_cases" = "darkred",
+                                "three_day_avg_deaths" = "darkblue")) 
+
+sne_plot
+
+ggsave("sne_plot.jpg", sne_plot, width = 11, height = 8.5)
+ggplotly(sne_plot)
+max(sne$date)
+
+sne_plot <- sne %>%
+  filter(state == "Rhode Island", date > ymd("2021-01-01")) %>%
+  select(date, state, daily_cases, daily_deaths) %>%
+  pivot_longer(cols = daily_cases:daily_deaths, 
+               names_to = "variable", values_to = "value") %>%
+  ggplot(aes(x = date, y = value)) +
+  geom_line(size = 1, aes(color = variable), show.legend = FALSE) +
+  facet_grid(variable ~ state, scales = "free_y", 
+             labeller = labeller(variable = variable_lab)) +
+  labs(x = "Date", y = "", title = "MA, CT, RI COVID-19") +
+  theme_ipsum_rc() +
+  scale_color_manual(values = c("daily_cases" = "darkred",
+                                "daily_deaths" = "darkblue")) 
+
+ggplotly(sne_plot)
