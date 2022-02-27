180370dce289d60ddca7c91321c06f43e9317189
Hollister <Hollister.Jeff@epa.gov>
2022-02-27 11:36:43 -0500

    '2022-02-27update.'

--- ri_doh_data.R
+++ ri_doh_data.R
@@ -5,27 +5,45 @@ library(tidyr)
 library(hrbrthemes)
 library(plotly)
 
+jwh_covid_risk_index <- function(cases, deaths, hosp, pos){
+  case_prop <- cases/max(cases, na.rm = TRUE)
+  death_prop <- deaths/max(deaths, na.rm = TRUE)
+  hosp_prop <- hosp/max(hosp, na.rm = TRUE)
+  pos_prop <- pos/max(pos, na.rm = TRUE)
+  (case_prop + death_prop + hosp_prop + pos_prop)/4 
+}
+
 gs4_deauth()
-ri_doh_data <- read_sheet("https://docs.google.com/spreadsheets/d/1c2QrNMz8pIbYEKzMJL7Uh2dtThOJa2j1sSMwiDo5Gz4", "Trends")
+ri_doh_data <- read_sheet("https://docs.google.com/spreadsheets/d/1c2QrNMz8pIbYEKzMJL7Uh2dtThOJa2j1sSMwiDo5Gz4", "Trends", na = c("", "NA", "--"))
 
 ri_covid_data <- select(ri_doh_data, date = Date, 
                         cases = 'New cases (may count people more than once)', 
                         tests = 'Daily total tests completed (may count people more than once)',
                         deaths = 'Date of death',
-                        hospitalized = 'Currently hospitalized') %>%
+                        hospitalized = 'Currently hospitalized',
+                        hospitalization = 'New hospital admissions',
+                        cases_7day_per100k = 
+                          'Total new cases per 100,000 persons in the past 7 days',
+                        ) %>%
   mutate(positivity = (cases/tests) * 100, 
+         risk_index = jwh_covid_risk_index(cases, deaths, hospitalization, 
+                                           positivity) * 100,
+         cases_7day_per100k = unlist(cases_7day_per100k),
          seven_day_avg_cases = zoo::rollapply(.data$cases, 7, mean, 
                                               fill = "right", align = "right"),
          seven_day_avg_deaths = 
            zoo::rollapply(.data$deaths, 7, mean, fill = "right", 
                           align = "right"),
-         seven_day_avg_hospitalized = 
-           zoo::rollapply(.data$hospitalized, 7, mean, fill = "right", 
+         seven_day_avg_hospitalization = 
+           zoo::rollapply(.data$hospitalization, 7, mean, fill = "right", 
+                          align = "right"),
+         seven_day_avg_index = 
+           zoo::rollapply(.data$risk_index, 7, mean, fill = "right", 
                           align = "right"),
          seven_day_avg_positivity = 
            zoo::rollapply(.data$positivity, 7, mean, fill = "right", 
                           align = "right")) %>%
-  select(date, cases, deaths, positivity, starts_with("seven")) %>%
+  #select(date, cases, deaths, positivity, starts_with("seven")) %>%
   pivot_longer(cols = cases:seven_day_avg_positivity, 
                names_to = "variable",
                values_to = "value")
@@ -33,27 +51,35 @@ ri_covid_data <- select(ri_doh_data, date = Date,
 variable_lab <- c("cases" = "Daily Cases",
                   "seven_day_avg_cases" = "7-Day Avg. Cases", 
                   "seven_day_avg_deaths" = "7-Day Avg. Deaths",
-                  "seven_day_avg_hospitalized" = "7-day Avg. Hospitalized",
+                  "seven_day_avg_hospitalization" = "7-day Avg. Hospitalizations",
+                  "seven_day_avg_index" = "7-day Avg. Risk Index",
                   "seven_day_avg_positivity" = "7-day Avg. Positivity")
 
 ri_daily <- ri_covid_data %>%
-  filter(variable %in% c("cases", "deaths", "positivity")) %>%
-  mutate(variable = case_when(variable == "cases" ~
+  filter(variable %in% c("cases", "deaths", "hospitalization", "positivity", 
+                         "risk_index")) %>%
+  mutate(variable = case_when(variable == "risk_index" ~
+                                "seven_day_avg_index",
+                              variable == "cases" ~
                                 "seven_day_avg_cases",
                               variable == "deaths" ~
                                 "seven_day_avg_deaths",
                               variable == "positivity" ~
-                                "seven_day_avg_positivity"
+                                "seven_day_avg_positivity",
+                              variable == "hospitalization" ~
+                                "seven_day_avg_hospitalization"
                               ))
 
-ri_covid_data %>% filter(date >= lubridate::today() - 8, variable == "cases") %>% pull(value) %>% sum -> cases_7day
-
-cases_7day_per100k <- round(100000*(cases_7day/1095000), 0)
+cases_7day_per100k <- filter(ri_covid_data, date == max(date), 
+                             variable == "cases_7day_per100k") %>% 
+  pull(value) %>%
+  max()
 
 ri_plots <- ri_covid_data |>
-  filter(!variable %in% c("seven_day_avg_hospitalized", "cases", "deaths", "positivity")) |>
+  filter(variable %in% c("seven_day_avg_cases", "seven_day_avg_deaths", "seven_day_avg_positivity",
+                         "seven_day_avg_hospitalization", "seven_day_avg_index")) |>
   ggplot(aes(x = date, y = value)) +
-  geom_point(data = ri_daily, aes(x=date, y=value), size = 1, color = "grey70") +
+  #geom_point(data = ri_daily, aes(x = date, y = value), size = 1, color = "grey70") +
   geom_line(size = 1, aes(color = variable)) +
   facet_grid(variable ~ ., scales = "free", 
              labeller = labeller(variable = variable_lab)) +
@@ -61,7 +87,7 @@ ri_plots <- ri_covid_data |>
                                           max(ri_covid_data$date), ")"), subtitle = paste0("Cases per 100k in last 7 days: ", cases_7day_per100k)) +
   theme_ipsum_rc() +
   theme(legend.position = "none") +
-  scale_color_manual(values = c("grey30","darkred","darkblue"))
+  scale_color_manual(values = c("grey30","darkred","darkblue", "darkcyan", "darkorchid1"))
 ggsave("ri_plot.jpg", ri_plots, width = 11, height = 8.5)
 index <- ggplotly(ri_plots)  %>%
   layout(title = list(text = paste0("Rhode Island COVID-19 (as of: ", 
